5a1196e6b671c5631ce6c2e48ff131486ccf1748
Tikendra Maitry <tikendramaitry@gmail.com>
2018-06-24 00:35:36 +0530

    Adding files initially

---
+++ .svn/text-base/readme.txt.svn-base
@@ -0,0 +1,53 @@
+=== Compare Bar Lite ===
+Contributors: wpfruits, tikendramaitry
+Tags: before and after, match fixers, comparison bar, compare products, show before and after for doctors
+Requires at least: 3.0.8
+Tested up to: 4.7
+Stable tag: 1.0.0
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+
+This Plugin allows you to create different comparison or before/after blocks that you can either use for doctors to show before and after effects.
+
+== Description ==
+This Plugin allows you to create different comparison or before/after blocks that you can either use for doctors to show before and after effects of a surgery or you may want to show an upcoming match between two teams or two individuals. 
+* Adds a cool block to your page.
+* You can create any number of compare bars and use them on your pages using the shortcode.
+* You can customize it to great extent
+* Easily manageable back-end.
+* Comes with a widget to show the bars in the Sidebar
+* Last but not the least its FREE
+
+
+= Features =
+* Adds a cool block to your page.
+* You can create any number of compare bars and use them on your pages using the shortcode.
+* You can customize it to great extent
+* Easily manageable back-end.
+* Comes with a widget to show the bars in the Sidebar
+* Last but not the least its FREE
+
+
+== Installation ==  
+-Upload the plugin folder to the /wp-content/plugins/ folder of your wordpress installation.
+-Activate the plugin. 
+-You will see an option at the left bottom of the wordpress options in the left sidebar.
+-Click on it to set the options
+-Then just create a new comparebar or edit the existing one. 
+-Click on edit and add the options to for the compare bar you have created and get going. 
+-Use the shortcode to display it on the page/post or you can use the widget if you want to show them in the sidebar
+
+
+== Frequently asked questions ==
+
+
+== Changelog ==
+
+= 1.0.0 =
+* Initial release
+
+
+== Upgrade notice ==
+
+
+== Arbitrary section ==
---
+++ comparebar.php
@@ -0,0 +1,95 @@
+<?php
+/*
+Plugin Name: Compare Bar Lite
+Plugin URI: http://wpfruits.com
+Description: This plugin will show the comparision between two members
+Author: wpfruits, tikendramaitry
+Version: 1.0.0
+Author URI: http://wpfruits.com
+*/
+//----------------------------------------------------------------------------------
+/*---Load Required Files------------------------------
+----------------------------------------------------*/
+include_once('scripts.php');
+include_once('inc/admin/cb_admin.php');
+//---------------------------------------------------
+
+function compbar_plugin_admin_menu() {
+	add_menu_page('Add Compare Bar', 'Comparebar', 'publish_posts', 'comparebar','add_comparebar',plugins_url('inc/images/icon.png',__FILE__));
+	add_submenu_page('comparebar', 'Edit Comparebar', '', 'publish_posts','cbar_edit','edit_comparebar');
+}
+add_action('admin_menu', 'compbar_plugin_admin_menu');
+
+//This function will create new database fields with default values
+function cbar_defaults(){
+	    $default = array(
+		'bgcolor'=>'#750400',
+		'title' => 'This is the title',
+		'img_width' =>'155',
+		'img_height' =>'220',
+		'image1_title' => 'First Image',
+		'image1_path' => plugins_url('inc/images/fighter.png',__FILE__),
+		'image1_hoverDes' => 'This is first image description',
+		'image2_title' => 'Second Image',
+		'image2_path' => plugins_url('inc/images/fighter.png',__FILE__),
+		'image2_hoverDes' => 'This is second image description',
+		'compare_txt' => 'VS'
+		);
+return $default;
+}
+
+function comp_install(){
+    global $wpdb;
+	$table_name = $wpdb->prefix . "comparebar"; 
+		$sql = "CREATE TABLE " . $table_name . " (
+		  id mediumint(9) NOT NULL AUTO_INCREMENT,
+		  option_name VARCHAR(255) NOT NULL DEFAULT  'comparebar_options',
+		  active tinyint(1) NOT NULL DEFAULT  '0',
+		  PRIMARY KEY (`id`),
+          UNIQUE (
+                    `option_name`
+            )
+		);";
+		require_once(ABSPATH . 'wp-admin/includes/upgrade.php');
+		dbDelta($sql);
+}
+
+// Runs when plugin is activated and creates new database field
+register_activation_hook(__FILE__,'comparebar_plugin_install');
+
+add_action('admin_init', 'comparebar_plugin_redirect');
+function comparebar_plugin_activate() {
+    add_option('comparebar_plugin_do_activation_redirect', true);
+}
+
+function comparebar_plugin_redirect() {
+    if (get_option('comparebar_plugin_do_activation_redirect', false)) {
+        delete_option('comparebar_plugin_do_activation_redirect');
+        wp_redirect('admin.php?page=comparebar');
+    }
+}
+
+function comparebar_plugin_install() {
+    add_option('comparebar_options', cbar_defaults());
+	$cbar_version = get_option('cbar_version'); 
+	if(!$cbar_version)
+	add_option('cbar_version',cbar_get_version());
+    comp_install();
+    global $wpdb;
+	$table_name = $wpdb->prefix . "comparebar"; 
+    $sql = "INSERT INTO " . $table_name . " values ('','comparebar_options','1');";
+    $wpdb->query( $sql );
+	comparebar_plugin_activate();
+}
+
+// get notificationbar version
+function cbar_get_version(){
+	if ( ! function_exists( 'get_plugins' ) )
+	require_once( ABSPATH . 'wp-admin/includes/plugin.php' );
+	$plugin_folder = get_plugins( '/' . plugin_basename( dirname( __FILE__ ) ) );
+	$plugin_file = basename( ( __FILE__ ) );
+	return $plugin_folder[$plugin_file]['Version'];
+}
+include_once('inc/front/cb_front.php');
+
+//----------------------------------------------------------------------------------
